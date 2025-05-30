17cd74e4004e5d3e00faf7c611a88f951df700a6
Kevin Alegria <kdaniel.vasquez24@itca.edu.sv>
2025-05-30 11:20:02 -0600

    pruebas

--- replit.nix
+++ replit.nix
@@ -2,6 +2,9 @@
 pkgs.mkShell {
   buildInputs = [
     pkgs.php82
+    pkgs.php82Extensions.mysqlnd
+    pkgs.php82Extensions.pdo_mysql
+    pkgs.php82Extensions.fileinfo
     pkgs.composer
   ];
 }
